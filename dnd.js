2b05bbab9689e2cc22e2453ef9f210f4a24eeb4d
keaganwhiston <keagnova124@gmail.com>
2023-03-13 13:43:30 -0400

    added more functionality to dnd.html

--- dnd.js
+++ dnd.js
@@ -1,14 +1,53 @@
 
 
-function calc(){
+function calcFull(){
     var party = document.getElementById("psize").value;
     var food = document.getElementById("food").value;
     var water = document.getElementById("water").value;
 
     var result1 = food - party;
     var result2 = water - party;
+    if (result1 < 0 && result2 < 0){
+        document.getElementById("message").textContent = "You are out of food! You are out of water!";
+
+    } else if (result1 < 0) {
+        document.getElementById("message").textContent = "You are out of food!";
+    } else if (result2 < 0) {
+        document.getElementById("message").textContent = "You are out of water!";
+    } else {
+        document.getElementById("message").textContent = "";
+    }
 
     document.getElementById("food").value = result1;
     document.getElementById("water").value = result2;
+
+}
+
+function calcHalf(){
+    var party = document.getElementById("psize").value;
+    var food = document.getElementById("food").value;
+
+    var result = food - (party/2);
+    if (result < 0) {
+        document.getElementById("message").textContent = "You are out of food!";
+    } else {
+        document.getElementById("message").textContent = "";
+    }
+
+    document.getElementById("food").value = result;
+}
+
+
+
+function calcWater(){
+    var party = document.getElementById("psize").value;
+    var water = document.getElementById("water").value;
+    var result = water - party;
+    if (result < 0) {
+        document.getElementById("message").textContent = "You are out of water!";
+    } else {
+        document.getElementById("message").textContent = "";
+    }
+    document.getElementById("water").value = result;
 
 }

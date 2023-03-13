a832fdfa0f03767dd65cb98eebe1e3df29809940
keaganwhiston <keagnova124@gmail.com>
2023-03-13 14:16:56 -0400

    added more functionality to the dnd page allowing the user to increment and decrement food and water

--- dnd.js
+++ dnd.js
@@ -26,28 +26,59 @@ function calcFull(){
 function calcHalf(){
     var party = document.getElementById("psize").value;
     var food = document.getElementById("food").value;
+    var water = document.getElementById("water").value;
 
-    var result = food - (party/2);
-    if (result < 0) {
+    var result1 = food - (party/2);
+    if (result1 < 0 && water < 0){
+        document.getElementById("message").textContent = "You are out of food! You are out of water!";
+
+    } else if (result1 < 0) {
         document.getElementById("message").textContent = "You are out of food!";
     } else {
         document.getElementById("message").textContent = "";
     }
 
-    document.getElementById("food").value = result;
+    document.getElementById("food").value = result1;
 }
 
 
 
 function calcWater(){
     var party = document.getElementById("psize").value;
+    var food = document.getElementById("food").value;
     var water = document.getElementById("water").value;
-    var result = water - party;
-    if (result < 0) {
+    var result2 = water - party;
+    if (food < 0 && result2 < 0){
+        document.getElementById("message").textContent = "You are out of food! You are out of water!";
+
+    } else if (result2 < 0) {
         document.getElementById("message").textContent = "You are out of water!";
     } else {
         document.getElementById("message").textContent = "";
     }
-    document.getElementById("water").value = result;
+    document.getElementById("water").value = result2;
 
+}
+
+function incrementFood() {
+    var foodInput = document.getElementById("food");
+    var currentValue = parseFloat(foodInput.value);
+    foodInput.value = (currentValue + 0.5).toFixed(1);
+}
+
+function decrementFood() {
+    var foodInput = document.getElementById("food");
+    var currentValue = parseFloat(foodInput.value);
+    foodInput.value = (currentValue - 0.5).toFixed(1);
+}
+function incrementWater() {
+    var waterInput = document.getElementById("water").value;
+    var change = parseInt(waterInput) + 1;
+    document.getElementById("water").value = change;
+}
+
+function decrementWater() {
+    var waterInput = document.getElementById("water").value;
+    var change = parseInt(waterInput) - 1;
+    document.getElementById("water").value = change;
 }
